9ffbef2e98fd5604aac59753bcc0ca23df4190ca
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-07-23 05:24:52 +0200

    changing files for deployment

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,4 +1,6 @@
 // const baseUrlLocal = "http://localhost:5000";
 
-const baseUrl = process.env.REACT_APP_API_URL;
-export default baseUrl;
+// const baseUrl = process.env.REACT_APP_API_URL;
+const baseUrlDeployment = process.env.REACT_APP_API_URL_Deployment;
+
+export default baseUrlDeployment;
