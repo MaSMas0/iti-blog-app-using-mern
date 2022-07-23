0054ef7b3e9aae12af9bd098da94ab9f6002c892
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-07-23 06:07:54 +0200

    preparing for deployment

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,6 +1,6 @@
-// const baseUrlLocal = "http://localhost:5000";
+const baseUrlLocal = "http://localhost:5000";
 
 // const baseUrl = process.env.REACT_APP_API_URL;
-const baseUrlDeployment = 'https://iti-mern-blog-app.herokuapp.com';
+// const baseUrlDeployment = 'https://iti-mern-blog-app.herokuapp.com';
 
-export default baseUrlDeployment;
+export default baseUrlLocal;
