8fe6dd43b5c477a834e7d15031c412208221116e
Mohamed Abd El-Samie Ahmad Mansour <masmas04@gmail.com>
2022-07-23 05:36:38 +0200

    changing files for deployment

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,6 +1,6 @@
 // const baseUrlLocal = "http://localhost:5000";
 
 // const baseUrl = process.env.REACT_APP_API_URL;
-const baseUrlDeployment = process.env.REACT_APP_API_URL_Deployment;
+const baseUrlDeployment = 'https://iti-mern-blog-app.herokuapp.com';
 
 export default baseUrlDeployment;
